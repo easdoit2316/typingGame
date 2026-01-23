1367395c882228ee08321acc1d620bf2106ed4f6
easdoit2316 <easdoit2316@gmail.com>
2026-01-23 19:40:47 +0900

    데이터 로드 성공

--- Assets/Scripts/WordManager.cs
+++ Assets/Scripts/WordManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class WordManager : MonoBehaviour
 {
     public static WordManager instance;
+
     private void Awake()
     {
         if (instance == null)
@@ -16,4 +18,27 @@ public class WordManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void Start()
+    {
+
+        string filePath = "Assets/Scripts/Words.txt";
+        string textFile = File.ReadAllText(filePath);
+        if(textFile != null)
+        {
+            string fileContents = textFile;
+            Debug.Log("File contents : " + fileContents);
+
+            string[] lines = fileContents.Split(", ");
+            foreach(string line in lines)
+            {
+                Debug.Log("Line : " + line);
+            }
+        }
+        else
+        {
+            Debug.LogError("Text file asset not assigned");
+        }
+    }
+
 }
